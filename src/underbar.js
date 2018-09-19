3b81278d03067fb1164c88eac11453231d72b3ec
jcelder <jcelder88@gmail.com>
2018-09-18 21:45:52 -0700

    Complete invoke function stub

--- src/underbar.js
+++ src/underbar.js
@@ -389,6 +389,12 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    return _.map(collection, function(elem) {
+      if (typeof(functionOrKey) === 'string') {
+        return elem[functionOrKey]();
+      }
+      return functionOrKey.apply(elem, args);
+    });
   };
 
   // Sort the object's values by a criterion produced by an iterator.
